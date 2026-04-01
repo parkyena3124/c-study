5748444643b02c7b5dc12ce08fb6f00831ffdf3e
parkyena <parkdice3124@gmail.com>
2026-04-01 15:21:38 +0900

    반복문

---
+++ self-learning/control.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+
+int main(void) {
+	int cnt = 0;
+	for (int i = 1; ; i++) {
+		if (i > 10) {
+			break;
+		}
+		if (i % 3 == 0) {
+			cnt++;
+		}
+	}
+	printf("3의 배수 개수: %d\n", cnt);
+	
+	return 0;
+}
+//결과: 3의 배수 개수: 3
