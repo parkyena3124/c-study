0dd7e17b77358da15bdc3785e8e4df5141eec1cd
parkyena <parkdice3124@gmail.com>
2026-04-01 15:23:29 +0900

    행열의 총합

---
+++ self-learning/arrayex.c
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+
+int main(void) {
+
+	int su[3][2] = { 33, 22, 44, 66, 77, 100 };
+	int sum = 0;
+	for (int i = 0; i < 3;i++) {
+		for (int j = 0;j < 2;j++) {
+			sum += su[i][j];
+		}
+	}
+	printf("합: %d", sum);
+}
+//결과: 합: 342
