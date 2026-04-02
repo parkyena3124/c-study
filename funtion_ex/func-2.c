06df31c550337bba2e0e164f51f6636fd91de248
parkyena <parkdice3124@gmail.com>
2026-04-02 12:49:25 +0900

    함수2

---
+++ funtion_ex/func-2.c
@@ -0,0 +1,22 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int fun1();
+int fun2();
+
+int main(void) {
+	printf("메인 시작\n"); //1
+	fun1(); //2
+	printf("메인 끝\n"); //10
+	return 0; //11
+}
+int fun1() { //(2)
+	printf("fun1함수 시작\n"); //3
+	fun2(); //4
+	printf("fun1함수 끝\n"); //8
+	return 0; //9
+}
+int fun2() { //(4)
+	printf("fun2함수 시작\n"); //5
+	printf("fun2함수 끝\n"); //6
+	return 0; //7
+}
